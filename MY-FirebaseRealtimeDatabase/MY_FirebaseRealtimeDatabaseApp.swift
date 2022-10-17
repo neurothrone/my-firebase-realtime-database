19d2cfe30afc3f24d7f0cfeb4b6ae26676b65ffa
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-18 00:52:27 +0200

    Initial Commit

---
+++ MY-FirebaseRealtimeDatabase/MY_FirebaseRealtimeDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MY_FirebaseRealtimeDatabaseApp.swift
+//  MY-FirebaseRealtimeDatabase
+//
+//  Created by Zaid Neurothrone on 2022-10-18.
+//
+
+import SwiftUI
+
+@main
+struct MY_FirebaseRealtimeDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
