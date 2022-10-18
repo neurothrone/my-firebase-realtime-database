35159333d9d7fd57fe67d826fc9f0923cfc52780
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-18 02:00:51 +0200

    finish first lecture walkthrough

--- MY-FirebaseRealtimeDatabase/ContentView.swift
+++ MY-FirebaseRealtimeDatabase/ContentView.swift
@@ -5,22 +5,97 @@
 //  Created by Zaid Neurothrone on 2022-10-18.
 //
 
+import Firebase
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+  @State private var allFruit: [String] = []
+  @State private var fruitName = ""
+  
+  private var ref: DatabaseReference! = Database.database().reference()
+  private let fruitCollection = "fruits"
+  
+  var body: some View {
+    VStack {
+      HStack {
+        TextField("Fruit name", text: $fruitName)
+          .textFieldStyle(.roundedBorder)
+          .autocorrectionDisabled(true)
+        
+        Button(action: addFruit) {
+          Image(systemName: "plus")
+            .imageScale(.large)
         }
-        .padding()
+        .buttonStyle(.borderedProminent)
+        .tint(.mint)
+        .disabled(fruitName.isEmpty)
+      }
+      .padding(.bottom)
+      
+      if allFruit.isEmpty {
+        Text("No fruits yet...")
+      } else {
+        List {
+          ForEach(allFruit, id: \.self) { fruit in
+            Text(fruit)
+          }
+          .onDelete(perform: deleteFruit)
+        }
+      }
+      
+      Spacer()
     }
+    .padding()
+    .onAppear(perform: populateFruit)
+    .refreshable {
+      Task {
+        populateFruit()
+      }
+    }
+  }
+}
+
+extension ContentView {
+  private func addFruit() {
+    ref.child(fruitCollection).childByAutoId().setValue(fruitName)
+    populateFruit()
+  }
+  
+  private func populateFruit() {
+    DispatchQueue.main.async {
+      withAnimation(.easeInOut) {
+        allFruit = fetchFruit()
+      }
+    }
+  }
+  
+  private func fetchFruit() -> [String] {
+    var fetchedFruit: [String] = []
+    
+    ref.child(fruitCollection).getData { error, snapshot in
+      if let error {
+        assertionFailure("❌ -> Failed to fetch fruit from Firebase. Error: \(error)")
+      }
+      
+      if let snapshot {
+        fetchedFruit = snapshot.children.compactMap {
+          let fruitSNap = $0 as! DataSnapshot
+          let fruitName = fruitSNap.value as! String
+          return fruitName
+        }
+      }
+    }
+    
+    return fetchedFruit
+  }
+  
+  private func deleteFruit(atOffsets: IndexSet) {
+    
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- MY-FirebaseRealtimeDatabase/MY_FirebaseRealtimeDatabaseApp.swift
+++ MY-FirebaseRealtimeDatabase/MY_FirebaseRealtimeDatabaseApp.swift
@@ -5,13 +5,27 @@
 //  Created by Zaid Neurothrone on 2022-10-18.
 //
 
+import FirebaseCore
+import Firebase
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+  ) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct MY_FirebaseRealtimeDatabaseApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
